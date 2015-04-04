1d8e444d1f42539ea2895648dfe25f3f15c1b926
Ethan Yuxuan Chen <ychen18@nd.edu>
2015-04-03 23:58:29 -0400

    add another label to sync with the first label and a decrement button

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -12,11 +12,12 @@ class ViewController: UIViewController {
 
     var count = 0
     var label:UILabel!
+    var label2:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Label
+        // Labels
         var label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)
         label.text = "0"
@@ -24,19 +25,41 @@ class ViewController: UIViewController {
         self.view.addSubview(label)
         self.label = label
         
+        var label2 = UILabel()
+        label2.frame = CGRectMake(200, 150, 60, 60)
+        label2.text = "0"
+        
+        self.view.addSubview(label2)
+        self.label2 = label2
+        
         // Button
         var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
+        button.frame = CGRectMake(150, 250, 120, 60)
+        button.setTitle("Increment", forState: .Normal)
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
         self.view.addSubview(button)
         
+        var button2 = UIButton()
+        button2.frame = CGRectMake(150, 300, 120, 60)
+        button2.setTitle("Decrement", forState: .Normal)
+        button2.setTitleColor(UIColor.redColor(), forState: .Normal)
+        self.view.addSubview(button2)
+        
         button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        button2.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
     }
     
     func incrementCount() {
         self.count++
         self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+    }
+    
+    func decrementCount() {
+        if self.count == 0 { return }
+        self.count--
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
     }
 }
 
